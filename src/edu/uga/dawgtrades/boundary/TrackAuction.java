559a0e5c5eb5c09b1e279e22032dd3cab518e37a
Jason Kaine <kainejas@uga.edu>
2014-12-10 06:03:28 -0500

    nsg

--- src/edu/uga/dawgtrades/boundary/TrackAuction.java
+++ src/edu/uga/dawgtrades/boundary/TrackAuction.java
@@ -135,7 +135,7 @@ public class TrackAuction
             root.put("auctions", tempList);
         }
         catch(Exception e) {
-            DawgTradesError.error( cfg, toClient, "Error making tempList for Page   " + e.getStackTrace() );
+            DawgTradesError.error( cfg, toClient, "Error making tempList for Page   " + e.getMessage() );
         }
       
             
